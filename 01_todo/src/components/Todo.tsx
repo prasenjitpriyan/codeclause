3fc38de17402b434d9cccc6428af65dee39b32a5
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-02 22:40:35 +0530

    Todo count types added

--- 01_todo/src/components/Todo.tsx
+++ 01_todo/src/components/Todo.tsx
@@ -1,34 +1,32 @@
 import { MdDeleteForever } from "react-icons/md";
 
+interface TodoCountTypes {
+  id: number;
+  text1: string;
+  text2: string;
+}
+
+let todoCountTypes: TodoCountTypes[] = [
+  { id: Math.random(), text1: "Remaining", text2: "01" },
+  { id: Math.random(), text1: "Completed", text2: "01" },
+  { id: Math.random(), text1: "Total", text2: "01" },
+];
+
 const Todo = () => {
   return (
     <section className="min-h-[75svh] grid grid-col-12 gap-5 p-5">
       <div className="col-start-1 col-span-1">
         <div className="flex flex-col justify-between gap-5">
-          <div className="flex flex-row justify-between">
-            <p className="w-[75%] bg-shortColor p-2 rounded-xl text-center text-primaryColor">
-              Remaining
-            </p>
-            <p className="w-[25%] bg-secondaryColor p-2 rounded-xl text-center">
-              01
-            </p>
-          </div>
-          <div className="flex flex-row justify-between">
-            <p className="w-[75%] bg-shortColor p-2 rounded-xl text-center text-primaryColor">
-              Completed
-            </p>
-            <p className="w-[25%] bg-secondaryColor p-2 rounded-xl text-center">
-              01
-            </p>
-          </div>
-          <div className="flex flex-row justify-between">
-            <p className="w-[75%] bg-shortColor p-2 rounded-xl text-center text-primaryColor">
-              Total
-            </p>
-            <p className="w-[25%] bg-secondaryColor p-2 rounded-xl text-center">
-              01
-            </p>
-          </div>
+          {todoCountTypes.map((todoCount) => (
+            <div key={todoCount.id} className="flex flex-row justify-between">
+              <p className="w-[75%] bg-shortColor p-2 rounded-xl text-center text-primaryColor">
+                {todoCount.text1}
+              </p>
+              <p className="w-[25%] bg-secondaryColor p-2 rounded-xl text-center">
+                {todoCount.text2}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
       <div className="col-start-3 col-span-9 w-[66%]">
