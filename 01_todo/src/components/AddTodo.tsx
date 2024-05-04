f3603c78cb7c753a273eab67b71a632757dd696e
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-04 17:01:55 +0530

    Addtodo completed

--- 01_todo/src/components/AddTodo.tsx
+++ 01_todo/src/components/AddTodo.tsx
@@ -1,6 +1,24 @@
-import * as React from "react";
+import React, { useState } from "react";
+import { useGlobalContext } from "../context/TodoContext";
 
 const AddTodo: React.FC = () => {
+  const { dispatch } = useGlobalContext();
+  const [value, setValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const addItem = () => {
+    if (value.length > 0) {
+      dispatch({
+        type: "ADD_TODO",
+        payload: value,
+      });
+      setValue("");
+    }
+  };
+
   return (
     <header className="h-[25svh] bg-secondaryColor w-full flex flex-col justify-evenly items-center">
       <h1 className="text-4xl text-primaryColor font-bold">Today</h1>
@@ -12,9 +30,14 @@ const AddTodo: React.FC = () => {
             type="text"
             placeholder="Add task..."
             className="p-2 w-[40vw] rounded-md border-none bg-primaryColor"
+            value={value}
+            onChange={handleChange}
           />
         </div>
-        <button className="bg-todoColor text-primaryColor p-2 rounded-md">
+        <button
+          className="bg-todoColor text-primaryColor p-2 rounded-md"
+          onClick={addItem}
+        >
           Add Task
         </button>
       </form>
