395ca306b29f3a9100a95dab7680de880927ca59
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-10 09:13:25 +0530

    translator app ui complete

--- 02_translator/src/App.jsx
+++ 02_translator/src/App.jsx
@@ -1,7 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Translator from "./components/Translator";
+
 function App() {
   return (
     <React.Fragment>
-      <h1 className="text-red-500">Hello</h1>
+      <Header />
+      <div className="flex flex-col justify-center items-center min-h-[90svh] bg-[#c73659]">
+        <Translator />
+      </div>
     </React.Fragment>
   );
 }
---
+++ 02_translator/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-center items-center bg-[#a91d3a] h-[10svh]">
+      <h1 className="text-[#eeeeee] text-3xl">Translator</h1>
+    </header>
+  );
+};
+
+export default Header;
