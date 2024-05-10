a242c5a55c39bdfa54fc526bb4baea3178543085
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-10 20:51:54 +0530

    Api fetched

--- 02_translator/src/main.jsx
+++ 02_translator/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { APIContextProvider } from "./apiContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <APIContextProvider>
     <App />
-  </React.StrictMode>,
-)
+  </APIContextProvider>
+);
